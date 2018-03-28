59d95958f854beb136347fd11ce3a0ca29f4d9bb
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-03-28 12:32:04 -0600

    Added the imports for the art panel.

--- src/art/view/ArtPanel.java
+++ src/art/view/ArtPanel.java
@@ -1,8 +1,28 @@
 package art.view;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
+import java.util.Hashtable;
+
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import art.controller.ArtController;
 
 public class ArtPanel extends JPanel
 {
-
+	private final int MINIMUM_EDGE = 5;
+	private final int MAXIMUM_EDGE = 20;
+	private final int MINIMUM_SCALE = 20;
+	private final int MAXIMUM_SCALE = 100;
+	
+	private ArtController app;
+	
 }

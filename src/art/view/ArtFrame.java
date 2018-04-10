59ded282496e091e6d9eeb42f28bd8982736e26a
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-04-10 12:56:11 -0600

    Added the frame code.

--- src/art/view/ArtFrame.java
+++ src/art/view/ArtFrame.java
@@ -5,5 +5,21 @@ import javax.swing.JFrame;
 
 public class ArtFrame extends JFrame
 {
-
+	private ArtPanel appPanel;
+	
+	public ArtFrame(ArtController app)
+	{
+		super();
+		appPanel = new ArtPanel(app);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame() 
+	{
+		this.setSize(1200, 700);
+		this.setContentPane(appPanel);
+		this.setTitle("Creating modern art in Java");
+		this.setVisible(true);
+	}
 }

309fc96a2e1b36468644a4708f3021fae5e42845
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-04-10 13:15:04 -0600

    Added some panel methods to allow creation of an object.

--- src/art/controller/ArtController.java
+++ src/art/controller/ArtController.java
@@ -1,10 +1,14 @@
 package art.controller;
 
+import art.view.ArtFrame;
+
 public class ArtController
 {
+	private ArtFrame appFrame;
+	
 	public ArtController()
 	{
-		
+		appFrame = new ArtFrame(this);
 	}
 	
 	public void start()
--- src/art/view/ArtPanel.java
+++ src/art/view/ArtPanel.java
@@ -89,6 +89,7 @@ public class ArtPanel extends JPanel
 		scaleSlider.setMajorTickSpacing(10);
 		scaleSlider.setPaintTicks(true);
 		scaleSlider.setPaintLabels(true);
+		scaleSlider.setValue(MINIMUM_SCALE);
 		
 		edgeSlider.setLabelTable(edgeLabels);
 		edgeSlider.setOrientation(JSlider.VERTICAL);
@@ -131,5 +132,47 @@ public class ArtPanel extends JPanel
 	private void setupListeners()
 	{
 		
+	}
+	
+	private boolean coinFlip()
+	{
+		return (int)(Math.random() * 2) == 0;
+	}
+	
+	private Polygon createPolygon(int sides)
+	{
+		Polygon currentShape = new Polygon();
+		
+		int originX = (int)(Math.random() * 600);
+		int originY = (int)(Math.random() * 600);
+		
+		for(int index = 0; index < sides; index++)
+		{
+			int minus = coinFlip() ? -1 : 1;
+			int shiftX = (int)(Math.random()  * currentScale) * minus;
+			minus = coinFlip() ? -1 : 1;
+			int shiftY = (int)(Math.random() * currentScale) * minus;
+			currentShape.addPoint(originX + shiftX, originY + shiftY);
+		}
+		
+		return currentShape;
+	}
+	
+	private Rectangle createRectangle()
+	{
+		Rectangle idk = new Rectangle();
+		
+		return idk;
+	}
+	
+	private Ellipse2D createEllipse()
+	{
+		Ellipse2D ellipse = new Ellipse2D.Double();
+		
+		int cornerX = (int)(Math.random() * 600);
+		int cornerY = (int)(Math.random() * 600);
+		double width = Math.random() * currentScale + 1;
+		
+		return ellipse;
 	}
 }
